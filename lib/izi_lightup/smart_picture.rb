1e605a9883233eaa549a50a8da8f09fb1af81d55
Izik AJ <izikaj@gmail.com>
2021-02-16 13:43:26 +0200

    refactor smart picture helper

--- lib/izi_lightup/smart_picture.rb
+++ lib/izi_lightup/smart_picture.rb
@@ -19,7 +19,7 @@ module IziLightup
             next unless version_name == :default || item.data.respond_to?(version_name)
 
             version = version_name == :default ? item.data : item.data.public_send(version_name)
-            next unless version&.file&.exists? && version&.url&.present?
+            next unless version_exists?(version)
 
             url = version.url
             dimensions = safe_dimentions(item, version)
@@ -37,15 +37,29 @@ module IziLightup
 
       private
 
-      def safe_dimentions(item, version)
-        dimensions = nil
-        begin
-          dimensions = version.dimensions
-        rescue MiniMagick::Error => _e
-          dimensions = [item&.width, item&.height].compact
-        end
+      def version_exists?(version)
+        return true if version&.url&.present? && version.url =~ /https?:/
 
-        return {} if dimensions.blank?
+        version&.file&.exists? && version&.url&.present?
+      end
+
+      def fallback_dimentions(item)
+        [item&.width, item&.height].compact
+      end
+
+      def safe_dimentions_extract(item, version)
+        begin
+          version.dimensions
+        rescue MiniMagick::Error => _e
+          fallback_dimentions(item)
+        end
+      end
+
+      def safe_dimentions(item, version)
+        dimensions = fallback_dimentions(item) if version.url =~ /https?:/
+        dimensions ||= safe_dimentions_extract(item, version)
+
+        return {} if dimensions.blank? || (dimensions.size != 2)
 
         %i[width height].zip(dimensions).to_h
       end
