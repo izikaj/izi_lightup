1434e08a6ebdf6c5f3fa305fd7dba552edf23cf0
Izik AJ <izikaj@gmail.com>
2021-08-16 12:19:26 +0300

    add JS uglification on release

--- release.sh
+++ release.sh
@@ -1,8 +1,13 @@
 version=$(cat lib/izi_lightup/version.rb | grep 'VERSION' | sed -e "s:.* = '::" | sed -e "s:'::")
 echo "Release version: $version"
-tagget=$(git tag | grep "v$version")
+tagged=$(git tag | grep "v$version")
 
-if [ "$tagget" != "v$version" ]
+# ruby uglify.rb
+ruby uglify.rb
+git add "app/assets/javascripts/crit-utils/*.min.js"
+git commit -m "refresh uglified utils"
+
+if [ "$tagged" != "v$version" ]
 then
   echo "Git release v$version not created, creating one..."
   git tag -a "v$version" -m "release v$version"
